af9ff759a6f6bcc01d372c3f25cb3bfc23ffe76d
Andrew Sullivan <andrew@sllvn.me>
2022-12-04 20:15:21 -0800

    Beginnings of move suggest

--- lib/bg_server_web/live/board.ex
+++ lib/bg_server_web/live/board.ex
@@ -15,26 +15,33 @@ defmodule BgServerWeb.Board do
       24 => %{black: 2},
     }
 
-    active_position = 18
-    dice_roll = {3, 5}
+    active_player = :black # the current session's player
+    turn = :black
+    active_position = 8
+    dice_roll = {3, 6}
 
     {:ok,
      assign(socket,
        positioned_pieces: positioned_pieces,
+       active_player: active_player,
+       turn: turn,
        active_position: active_position,
        dice_roll: dice_roll
      )}
   end
 
   def handle_event("set_active", value, socket) do
-    IO.inspect(value)
-    active_position = String.to_integer(value["position"])
-    # {:ok, new_temp} = Thermostat.inc_temperature(socket.assigns.id)
-    {:noreply,
-     assign(socket,
-       active_position: active_position
-    )}
+    click_position = String.to_integer(value["position"])
+    active_position = cond do
+      click_position == socket.assigns[:active_position] -> nil
+      true -> click_position
+    end
+
+    {:noreply, assign(socket, active_position: active_position)}
   end
+
+  def handle_event("move_piece", _value, _socket), do: :not_implemented
+  def handle_event("undo_move_piece", _value, _socket), do: :not_implemented
 
   defp cx_for_position(position) do
     base =
@@ -60,13 +67,17 @@ defmodule BgServerWeb.Board do
     |> Map.get(color, 0)
   end
 
-  defp cy_for_position(position, index, positioned_pieces, color) do
-    # _pieces_at_position = pieces_at_position(positioned_pieces, position, color)
+  defp cy_for_position(position, index, positioned_pieces) do
     if position <= 12 do
       800 - ((index - 1) * 70 + 35)
     else
       (index - 1) * 70 + 35
     end
+  end
+
+  defp cy_for_position(position, positioned_pieces) do
+    # used for possible moves
+    cy_for_position(position, Map.get(positioned_pieces, position, 1), positioned_pieces)
   end
 
   defp classes_for_piece(index, position, color, positioned_pieces, active_position) do
@@ -77,4 +88,12 @@ defmodule BgServerWeb.Board do
       true -> nil
     end
   end
+
+  defp possible_moves(nil, _dice_roll, _positioned_pieces), do: []
+  defp possible_moves(position, dice_roll, positioned_pieces) do
+    IO.inspect([position, dice_roll], label: "possible_moves")
+    [position - elem(dice_roll, 0), position - elem(dice_roll, 1)]
+  end
+
+  defp is_valid_move(), do: :not_implemented
 end
